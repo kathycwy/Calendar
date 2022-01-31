645226bf0a11da05e62e617e1edb92af3f67111c
cc.ys.chan@gmail.com <cc.ys.chan@gmail.com>
2022-01-31 12:49:15 +0100

    updated launch page and fixed bug in event list

--- Calendar/Helpers/AppIconHelper.swift
+++ Calendar/Helpers/AppIconHelper.swift
@@ -9,6 +9,19 @@
 import Foundation
 import UIKit
 
+
+extension Bundle {
+    public var icon: UIImage? {
+        if let icons = infoDictionary?["CFBundleIcons"] as? [String: Any],
+            let primaryIcon = icons["CFBundlePrimaryIcon"] as? [String: Any],
+            let iconFiles = primaryIcon["CFBundleIconFiles"] as? [String],
+            let lastIcon = iconFiles.last {
+            return UIImage(named: lastIcon)
+        }
+        return nil
+    }
+}
+
 class AppIconHelper {
     
     // MARK: - Properties
